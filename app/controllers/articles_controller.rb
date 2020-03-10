c61ae79c7cbd41d417da4626b7c401e568727132
谷崎公紀 <marutto8@gmail.com>
2020-03-10 14:51:42 +0900

    add 編集、削除機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,9 +20,24 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
-  private
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path 
+  end
+
+private
   def article_params
     params.require(:article).permit(:title, :body)
   end
-
 end
